87dfa0ae944e80b35f4324e02353844d97606c5f
jinyanwei <kingyeahwei@163.com>
2020-08-30 00:20:49 +0800

    promise

---
+++ src/js/Promise/demo.js
@@ -0,0 +1,30 @@
+// const promise = new Promise(function(resolve, reject) {
+//   if (/*异步操作成功*/) {
+//     resolve(value)
+//   } else {
+//     reject(error)
+//   }
+// })
+
+// promise.then(function(value) {}, function(error) {})
+
+// function timeout(ms) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(resolve, ms, 'done');
+//   })
+// }
+
+// timeout(100).then((value) => {
+//   console.log(value);
+// })
+
+// let promise = new Promise(function (resolve, reject) {
+//   console.log('promise');
+//   resolve()
+// })
+
+// promise.then(function () {
+//   console.log('resolved');
+// })
+
+// console.log('Hi!');
