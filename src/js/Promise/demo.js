035298068260c4460ead062496506a83b674b2bc
jinyanwei <kingyeahwei@163.com>
2020-08-30 02:18:33 +0800

    promise

--- src/js/Promise/demo.js
+++ src/js/Promise/demo.js
@@ -97,3 +97,154 @@
 //   return resolve(1);
 //   console.log(2);
 // })
+
+// getJSON("/posts.json")
+//   .then(function (json) {})
+//   .then(function (post) {});
+
+// getJSON('/post/1.json')
+//   .then(function (post) {
+//     return getJSON(post.commentURL);
+//   })
+//   .then(
+//     function (comments) {
+//       console.log('resolved: ', comments);
+//     },
+//     function (err) {
+//       console.log('reject: ', err);
+//     }
+//   );
+// p.then((value) => console.log('fulfilled:', val)).catch((err) =>
+//   console.log('rejected', err)
+// );
+
+// p.then((val) => console.log('fulfilled:' + val)).then(null, (err) =>
+//   console.log('rejected:', err)
+// );
+
+// const promise = new Promise(function (resolve, reject) {
+//   throw new Error('test')
+// });
+// promise.catch(function(error) {
+//   console.log(error);
+// })
+
+// const promise = new Promise(function (resolve, reject) {
+//   try {
+//     throw new Error('test');
+//   } catch (e) {
+//     reject(e);
+//   }
+// });
+// promise.catch(function (error) {
+//   console.log(error);
+// });
+
+// const promise = new Promise(function (resolve, reject) {
+//   reject(new Error('test'));
+// });
+
+// promise.catch(function (error) {
+//   console.log(error);
+// });
+
+// const promise = new Promise(function (resolve, reject) {
+//   resolve('ok');
+//   throw new Error('test');
+// });
+// promise
+//   .then(function (value) {
+//     console.log(value);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// getJSON('/post/1.json')
+//   .then(function (post) {
+//     return getJSON(post.commentURL);
+//   })
+//   .then(function (comments) {})
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// promise.then(
+//   function (data) {},
+//   function (error) {}
+// );
+
+// promise.then(function (data) {}).catch(function (err) {});
+
+// const someAsyncThing = function () {
+//   return new Promise(function (resolve, reject) {
+//     resolve(x + 2);
+//   });
+// };
+
+// someAsyncThing().then(function () {
+//   console.log('everything is great');
+// });
+
+// setTimeout(() => {
+//   console.log(123);
+// }, 2000);
+
+// const promise = new Promise(function (resolve, reject) {
+//   resolve('ok');
+//   setTimeout(() => {
+//     throw new Error('test');
+//   }, 0);
+// });
+// promise.then(function(value) {console.log(value);})
+
+// const someAsyncThing = function () {
+//   return new Promise(function (resolve, reject) {
+//     resolve(x + 2);
+//   });
+// };
+
+// someAsyncThing()
+//   .catch(function (error) {
+//     console.log('oh no', error);
+//   })
+//   .then(function () {
+//     console.log('carry on');
+//   });
+
+// Promise.resolve()
+//   .catch(function (error) {
+//     console.log('oh no ', error);
+//   })
+//   .then(function () {
+//     console.log('carry on');
+//   });
+
+// const someAsyncThing = function () {
+//   return new Promise(function (resolve, reject) {
+//     resolve(x + 2);
+//   });
+// };
+
+// someAsyncThing()
+//   .then(function () {
+//     return someAsyncThing();
+//   })
+//   .catch(function (error) {
+//     console.log('on no', error);
+//     y + 2;
+//   })
+//   .then(function () {
+//     console.log('carry on');
+//   });
+
+// someAsyncThing().then(function() {
+//   return someAsyncThing()
+// }).catch(function(error) {
+//   console.log('oh no ', error);
+//   y + 2
+// }).catch(function(error) {
+//   console.log('carry on', error);
+// })
+
+// note Promise.prototype.finally()
