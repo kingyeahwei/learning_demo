3c23d665ac2b42d9df6683ac355bb9c8d96830b9
jinyanwei <jinyanwei@onetarget.cn>
2020-08-31 18:42:22 +0800

    iterator

--- src/js/Iterator/demo.js
+++ src/js/Iterator/demo.js
@@ -13,3 +13,13 @@
 // console.log(it.next());
 // console.log(it.next());
 
+function makeIterator(array) {
+  var nextIndex = 0;
+  return {
+    next: function () {
+      return nextIndex < array.length
+        ? { value: array[nextIndex++] }
+        : { done: true };
+    },
+  };
+}
