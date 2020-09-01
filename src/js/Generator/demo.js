4e9074c2cef8c2447ed4c083e16a60d90055c1d9
jinyanwei <kingyeahwei@163.com>
2020-09-02 00:23:11 +0800

    Generator

--- src/js/Generator/demo.js
+++ src/js/Generator/demo.js
@@ -429,7 +429,322 @@
 
 // const gen = g(1, 2);
 // console.log(gen.next())
-// console.log(gen.throw(new Error('出错了'))); 
+// console.log(gen.throw(new Error('出错了')));
 // console.log(gen.return(2));
 
+// tag yield* 表达式
+// function* foo() {
+//   yield 'a';
+//   yield 'b';
+// }
 
+// function* bar() {
+//   yield 'x';
+//   for (const i of foo()) {
+//     console.log(i);
+//   }
+//   yield 'y'
+// }
+
+// for (const v of bar()) {
+//   console.log(v);
+// }
+
+// function* bar() {
+//   yield 'x';
+//   yield* foo();
+//   yield 'y'
+// }
+
+// function* bar() {
+//   yield 'x';
+//   yield 'a';
+//   yield 'b';
+//   yield 'y';
+// }
+
+// for (const v of bar()) {
+//   console.log(v);
+// }
+
+// function* bar() {
+//   yield 'x';
+//   for (const v of foo()) {
+//     yield v;
+//   }
+//   yield 'y'
+// }
+
+// for (const v of bar()) {
+//   console.log(v);
+// }
+
+// function* inner() {
+//   yield 'hello!';
+// }
+
+// function* outer1() {
+//   yield 'open';
+//   yield inner();
+//   yield 'close';
+// }
+
+// var gen = outer1();
+
+// console.log(gen.next().value)
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+
+// function* outer2() {
+//   yield 'open';
+//   yield* inner();
+//   yield 'close';
+// }
+
+// var gen = outer2()
+
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+
+// let deletatedIterator = (function* () {
+//   yield 'Hello!';
+//   yield 'Bye!';
+// })();
+
+// let delegatingIterator = (function* () {
+//   yield 'Greetings';
+//   yield* deletatedIterator;
+//   yield 'Ok, bye.';
+// })();
+
+// for (let value of delegatingIterator) {
+//   console.log(value);
+// }
+
+// function* concat(iter1, iter2) {
+//   yield* iter1;
+//   yield* iter2;
+// }
+
+// function* concat(iter1, iter2) {
+//   for (const value of iter1) {
+//     yield value;
+//   }
+//   for (const value of iter2) {
+//     yield value;
+//   }
+// }
+
+// function* gen() {
+//   yield* ['a', 'b', 'c'];
+// }
+
+// console.log(gen().next())
+
+// let read = (function* () {
+//   yield 'hello';
+//   yield* 'hello';
+// })();
+
+// console.log(read.next().value)
+// console.log(read.next().value)
+
+// function* foo() {
+//   yield 2;
+//   yield 3;
+//   return 'foo';
+// }
+
+// function* bar() {
+//   yield 1;
+//   var v = yield* foo();
+//   console.log('v: ' + v);
+//   yield 4;
+// }
+
+// var it = bar();
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+// console.log(it.next());
+
+// function* genFuncWithReturn() {
+//   yield 'a';
+//   yield 'b';
+//   return 'The result'
+// }
+
+// function* logReturned(genObj) {
+//   let result = yield* genObj;
+//   console.log(result);
+// }
+
+// console.log([...logReturned(genFuncWithReturn())]);
+
+// function* iterTree(tree) {
+//   if (Array.isArray(tree)) {
+//     for (let i = 0; i < tree.length; i++) {
+//       yield* iterTree(tree[i]);
+//     }
+//   } else {
+//     yield tree;
+//   }
+// }
+
+// const tree = ['a', ['b', 'c'], ['d', 'e']];
+
+// for (let x of iterTree(tree)) {
+//   console.log(x);
+// }
+
+// console.log(...iterTree(tree));
+
+// function Tree(left, label, right) {
+//   this.left = left;
+//   this.label = label;
+//   this.right = right;
+// }
+
+// function* inorder(t) {
+//   if (t) {
+//     yield* inorder(t.left);
+//     yield t.label;
+//     yield* inorder(t.right);
+//   }
+// }
+
+// function make(array) {
+//   if (array.length == 1) {
+//     return new Tree(null, array[0], null);
+//   }
+//   return new Tree(make(array[0]), array[1], make(array[2]));
+// }
+
+// let tree = make([
+//   [['a'], 'b', ['c']],
+//   'd',
+//   [['e'], 'f', ['g']]
+// ]);
+
+// var result = [];
+
+// for(let node of inorder(tree)) {
+//   result.push(node)
+// }
+
+// console.log(result);
+
+// tag 作为对象属性的generator函数
+// let obj = {
+//   * myGeneratorMethod() {
+
+//   }
+// }
+
+// let obj = {
+//   myGeneratorMethod: function* () {
+
+//   }
+// }
+
+// function* g() {}
+
+// g.prototype.hello = function () {
+//   return 'hi!';
+// };
+
+// let obj = g();
+
+// console.log(obj instanceof g);
+
+// console.log(obj.hello())
+
+// function* g() {
+//   this.a = 11;
+// }
+
+// let obj = g();
+
+// obj.next()
+// console.log(obj.a);
+
+// function* F() {
+//   yield this.x = 2;
+//   yield this.y = 3;
+// }
+
+// new F()
+// function* F() {
+//   this.a = 1;
+//   yield this.b = 2;
+//   yield this.c = 3;
+// }
+
+// var obj = {}
+// var f = F.call(obj);
+
+// console.log(f.next());
+// console.log(f.next());
+// console.log(f.next());
+
+// console.log(obj.a);
+// console.log(obj.b);
+// console.log(obj.c);
+
+// function* F() {
+//   this.a = 1;
+//   yield this.b = 2;
+//   yield this.c = 3;
+// }
+
+// var f = F.call(F.prototype);
+
+// console.log(f.next());
+// console.log(f.next());
+// console.log(f.next());
+
+// console.log(f.a);
+// console.log(f.b);
+// console.log(f.c);
+
+// function* gen() {
+//   this.a = 1;
+//   yield this.b = 2;
+//   yield this.c = 3;
+// }
+
+// function F() {
+//   return gen.call(gen.prototype);
+// }
+
+// var f = new F();
+
+// console.log(f.next());
+// console.log(f.next());
+// console.log(f.next());
+
+// console.log(f.a);
+// console.log(f.b);
+// console.log(f.c);
+
+// var ticking = true;
+// var clock = function() {
+//   if (ticking) {
+//     console.log('Tick!');
+//   } else {
+//     console.log('Tock!');
+//   }
+//   ticking = !ticking;
+// }
+
+// var clock = function* () {
+//   while (true) {
+//     console.log('Tick!');
+//     yield;
+//     console.log('Tock!');
+//     yield;
+//   }
+// };
+
+// note Generator与协程
