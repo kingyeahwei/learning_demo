2ecd02b255a73a9afd18e9795c4a648719934243
jinyanwei <jinyanwei@onetarget.cn>
2020-09-02 14:53:13 +0800

    generator应用

--- src/js/Generator/demo.js
+++ src/js/Generator/demo.js
@@ -748,3 +748,161 @@
 // };
 
 // note Generator与协程
+
+// function* gen() {
+//   yield 1;
+//   yield 2;
+// }
+
+// let g = gen();
+
+// console.log(g.next().value, g.next().value);
+
+// function* loadUI() {
+//   showLoadingScreen();
+//   yield loadUIDataAsynchronously();
+//   hideLoadingScreen();
+// }
+
+// var loader = loadUI();
+
+// loader.next();
+// loader.next()
+
+// function* main() {
+//   var result = yield request('http://some.url');
+//   var resp = JSON.parse(result);
+//   console.log(resp.value);
+// }
+
+// function request(url) {
+//   makeAjaxCall(url, function (response) {
+//     it.next(response);
+//   });
+// }
+
+// var it = main();
+// it.next();
+
+// function* numbers() {
+//   let file = new FileReader('numbers.txt');
+//   try {
+//     while (!file.eof) {
+//       yield parseInt(file.readLine(), 10);
+//     }
+//   } finally {
+//     file.close();
+//   }
+// }
+
+// step1(function (value1) {
+//   step2(value1, function (value2) {
+//     step3(value2, function (value3) {
+//       step4(value3, function (value4) {
+//         console.log(value4);
+//       });
+//     });
+//   });
+// });
+
+// Promise.resolve(step1)
+//   .then(step2)
+//   .then(step3)
+//   .then(step4)
+//   .then(
+//     function () {},
+//     function () {}
+//   )
+//   .done();
+
+// function* longRunningTask(value1) {
+//   try {
+//     var value2 = yield step1(value1);
+//     var value3 = yield step2(value2);
+//     var value4 = yield step3(value3);
+//     var value5 = yield step4(value4);
+//   } catch (e) {}
+// }
+
+// function scheduler(task) {
+//   var taskObj = task.next(task.value);
+//   if (!taskObj.done) {
+//     task.value = taskObj.value;
+//     scheduler(task);
+//   }
+// }
+
+// scheduler(longRunningTask(initialValue));
+
+// let steps = [step1Func, step2Func, step3Func];
+
+// function* iterateSteps(steps) {
+//   for (var i = 0; i < steps.length; i++) {
+//     var step = steps[i];
+//     yield step();
+//   }
+// }
+
+// let jobs = [jobs1, jobs2, jobs3];
+// function* iterateJobs(jobs) {
+//   for (var i = 0; i < jobs.length; i++) {
+//     var job = jobs[i];
+//     yield iterateSteps(job.steps);
+//   }
+// }
+
+// for (var step of iterateJobs(jobs)) {
+//   console.log(step.id);
+// }
+
+// var it = iterateJobs(jobs);
+// var res = it.next();
+
+// while (!res.done) {
+//   var result = res.value;
+//   res = it.next()
+// }
+
+// 部署Iterator接口
+// function* iterEntries(obj) {
+//   let keys = Object.keys(obj);
+//   for (let i = 0; i < keys.length; i++) {
+//     let key = keys[i];
+//     yield [key, obj[key]];
+//   }
+// }
+
+// let myObj = { foo: 3, bar: 7 };
+// for (let [key, value] of iterEntries(myObj)) {
+//   console.log(key, value);
+// }
+
+// function* makeSimpleGenerator(array) {
+//   var nextIndex = 0;
+//   while (nextIndex < array.length) {
+//     yield array[nextIndex++]
+//   }
+// }
+// var gen = makeSimpleGenerator(['yo', 'ya']);
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+// console.log(gen.next().done)
+
+// tag 作为数据结构
+// function* doStuff() {
+//   yield fs.readFile.bind(null, 'hello.txt');
+//   yield fs.readFile.bind(null, 'world.txt');
+//   yield fs.readFile.bind(null, 'and-such.txt');
+// }
+
+// for (task of doStuff()) {
+//   task()
+// }
+
+// function doStuff() {
+//   return [
+//     fs.readFile.bind(null, 'hello.txt'),
+//     fs.readFile.bind(null, 'world.txt'),
+//     fs.readFile.bind(null, 'and-such.txt'),
+//   ];
+// }
