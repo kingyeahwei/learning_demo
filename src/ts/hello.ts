ccf6bd23e876dc954fcb2d6061facd0e9f403b2d
jinyanwei <jinyanwei@onetarget.cn>
2020-09-22 12:44:12 +0800

    双重断言

--- src/ts/hello.ts
+++ src/ts/hello.ts
@@ -338,3 +338,180 @@
 //     return x.split('').reverse().join('');
 //   }
 // }
+
+// interface Cat {
+//   name: string;
+//   run(): void;
+// }
+
+// interface Fish {
+//   name: string;
+//   swim(): void;
+// }
+
+// function getName(animal: Cat | Fish) {
+//   return animal.name;
+// }
+
+// interface Cat {
+//   name: string;
+//   run(): void;
+// }
+
+// interface Fish {
+//   name: string;
+//   swim(): void;
+// }
+
+// function isFish(animal: Cat | Fish) {
+//   if (typeof (animal as Fish).swim === 'function') {
+//     return true;
+//   }
+//   return false;
+// }
+
+// interface Cat {
+//   name: string;
+//   run(): void;
+// }
+
+// interface Fish {
+//   name: string;
+//   swim(): void;
+// }
+
+// function swim(animal: Cat | Fish) {
+//   (animal as Fish).swim();
+// }
+
+// const tom: Cat = {
+//   name: 'Tom',
+//   run() {
+//     console.log('run');
+//   },
+// };
+
+// swim(tom)
+
+// class ApiError extends Error {
+//   code: number = 0;
+// }
+
+// class HttpError extends Error {
+//   statusCode: number = 200;
+// }
+
+// function isApiError(error: Error) {
+//   if (typeof (error as ApiError).code === 'number') {
+//     return true;
+//   }
+//   return false;
+// }
+
+// class ApiError extends Error {
+//   code: number = 0;
+// }
+
+// class HttpError extends Error {
+//   statusCode: number = 200;
+// }
+
+// function isApiError(error: Error) {
+//   if (error instanceof ApiError) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// interface ApiError extends Error {
+//   code: number;
+// }
+
+// interface HttpError extends Error {
+//   statusCode: number;
+// }
+
+// function isApiError(error: Error) {
+//   if (error instanceof ApiError) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// interface ApiError extends Error {
+//   code: number;
+// }
+
+// interface HttpError extends Error {
+//   statusCode: number;
+// }
+
+// function isApiError(error: Error) {
+//   if (typeof (error as ApiError).code === 'number') {
+//     return true;
+//   }
+//   return false;
+// }
+
+// const foo: number = 1;
+// foo.length = 1;
+
+// window.foo  = 1;
+
+// (window as any).foo = 1;
+
+// function getCacheData(key: string): any {
+//   return (window as any).cache[key];
+// }
+
+// interface Cat {
+//   name: string;
+//   run(): void;
+// }
+
+// const tom = getCacheData('tom') as Cat;
+
+// tom.run()
+
+// interface Animal {
+//   name: string;
+// }
+
+// interface Cat {
+//   name: string;
+//   run(): void;
+// }
+
+// let tom: Cat = {
+//   name: 'Tom',
+//   run: () => {
+//     console.log('run');
+//   },
+// };
+
+// let animal: Animal = tom;
+
+// interface Animal {
+//   name: string;
+// }
+
+// interface Cat extends Animal {
+//   run(): void;
+// }
+
+interface Animal {
+  name: string;
+}
+
+interface Cat {
+  name: string;
+  run(): void;
+}
+
+function testAnimal(animal: Animal) {
+  return animal as Cat;
+}
+
+function testCat(cat: Cat) {
+  return cat as Animal;
+}
